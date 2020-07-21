7fcba8b594a504d556a4ea8d6656f08882528c08
seyedalirezamousavi77 <s.alireza.m8888@gmail.com>
2020-07-21 12:37:21 +0430

    second commit

--- src/component/container/Container.jsx
+++ src/component/container/Container.jsx
@@ -10,6 +10,9 @@ function Container(props) {
         {
             id: 1, name: 'ali', family: 'bashe', phone: '091212345678', email: 'ali@gmail.com', address: 'khaniabad no', birthday: '1399/2/2', details: ':)'
         },
+        {
+            id: 2, name: 'ali', family: 'bashe', phone: '091212345678', email: 'ali@gmail.com', address: 'khaniabad no', birthday: '1399/2/2', details: ':)'
+        },
     ])
 
     const showForm = (check) => {
@@ -22,12 +25,15 @@ function Container(props) {
         // console.log(id);
         // console.log(contact.id)
         e.preventDefault();
-        setContact(e.target.value)
+        const { name, value } = e.target;
+        console.log(name);
+        console.log(value)
+        setContact([...contact, { [name]: value }])
     }
     const clearForm = () => {
 
     }
-
+    const {id , name , family , phone , email, address, birthday , details} = contact;
     return (
         <div>
             <>
@@ -60,26 +66,32 @@ function Container(props) {
                                         <th>action</th>
                                     </tr>
                                 </thead>
+                                
                                 {
-                                    contact.map(item => (
-                                        <tbody key={item.id}>
-                                            <tr>
-                                                <td>{item.id}</td>
-                                                <td>{item.name}</td>
-                                                <td>{item.family}</td>
-                                                <td>{item.phone}</td>
-                                                <td>{item.email}</td>
-                                                <td>{item.address}</td>
-                                                <td>{item.birthday}</td>
-                                                <td>{item.details}</td>
-                                                <td>
-                                                    <button type="submit" className="btn btn-primary">Save</button>
-                                                    <button type="reset" className="btn btn-primary">Clear</button>
-                                                </td>
-                                            </tr>
-                                        </tbody>
-                                    ))
+                                    contact.map(item => {
+                                        
+                                        return (
+                                            <tbody key={item.id}>
+                                                <tr>
+                                                    <td>{item.id}</td>
+                                                    <td>{item.name}</td>
+                                                    <td>{item.family}</td>
+                                                    <td>{item.phone}</td>
+                                                    <td>{item.email}</td>
+                                                    <td>{item.address}</td>
+                                                    <td>{item.birthday}</td>
+                                                    <td>{item.details}</td>
+                                                    <td>
+                                                        <button type="submit" className="btn btn-primary">Save</button>
+                                                        <button type="reset" className="btn btn-primary">Clear</button>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        )
+                                        
+                                    })
                                 }
+                                
                             </table>
                             <button className="btn btn-primary" onClick={(check => showForm(check))}>Add</button>
                             <button className="btn btn-secondary" onClick={check => hideForm(check)}>Cancel</button>
@@ -88,37 +100,37 @@ function Container(props) {
                             <form id='contact_form'>
                                 <div className="form-group">
                                     <label for="id_id">ID: </label>
-                                    <input value={contact.id} type="number" name="id" className="form-control" id="id_id" placeholder="id of contact" required />
+                                    <input value={id} type="number" name="id" className="form-control" id="id_id" placeholder="id of contact" required />
                                 </div>
 
                                 <div className="form-group">
                                     <label for="id_name">Name: </label>
-                                    <input type="text" name="name" className="form-control" id="id_name" placeholder="Enter name" required />
+                                    <input value={name} type="text" name="name" className="form-control" id="id_name" placeholder="Enter name" required />
                                 </div>
                                 <div className="form-group">
                                     <label for="id_last_name">Last name: </label>
-                                    <input type="text" className="form-control" name="lastName" id="id_last_name" placeholder="Enter last name" />
+                                    <input value={family} type="text" className="form-control" name="family" id="id_last_name" placeholder="Enter last name" />
                                 </div>
                                 <div className="form-group">
                                     <label for="id_phone">phone: </label>
-                                    <input type="phone" name="phone" className="form-control" id="id_phone" placeholder="Enter Phone" />
+                                    <input value={phone} type="phone" name="phone" className="form-control" id="id_phone" placeholder="Enter Phone" />
                                 </div>
                                 <div className="form-group">
                                     <label for="id_email">Email: </label>
-                                    <input type="email" name='email' class="form-control" id="id_email" placeholder="Enter Email" />
+                                    <input value={email} type="email" name='email' class="form-control" id="id_email" placeholder="Enter Email" />
                                 </div>
                                 <div class="form-group">
                                     <label for="id_address">address</label>
-                                    <textarea name="address" className="form-control" id="id_address" placeholder="Enter Address"></textarea>
+                                    <textarea value={address} name="address" className="form-control" id="id_address" placeholder="Enter Address"></textarea>
                                 </div>
                                 <div className="form-group">
                                     <label for="id_birthday">birthday</label>
-                                    <input type="datetime-local" name='birthday' className="form-control" id="id_birthday"
+                                    <input value={birthday} type="datetime-local" name='birthday' className="form-control" id="id_birthday"
                                         placeholder="Enter birthday" />
                                 </div>
                                 <div className="form-group">
                                     <label for="id_details">details: </label>
-                                    <input type="text" name="details" className="form-control" id="id_details" placeholder="Enter details" />
+                                    <input value={details} type="text" name="details" className="form-control" id="id_details" placeholder="Enter details" />
                                 </div>
                                 <button type="submit" className="btn btn-primary" onClick={submitForm}>Save</button>
                                 <button type="reset" className="btn btn-primary" onClick={clearForm}>Clear</button>
